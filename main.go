9a419daeb1a10b0e4a58c576ac4ecf91b7d53393
Kush-316 <kushagrakapoor619@gmail.com>
2025-03-21 18:09:58 +0530

    complete

--- main.go
+++ main.go
@@ -64,7 +64,13 @@ func updateMovie(w http.ResponseWriter, r *http.Request){
 	params := mux.Vars(r)
 	for index, item:= range movies{
 		if item.ID == params["id"]{
-			movies = append(movies[:index], movies[index+1:]...)
+			movies = append(movies[:index],movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
 		}
 	}
 }
@@ -93,7 +99,7 @@ func main() {
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("movies/{id}", updateMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000")
